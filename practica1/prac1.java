c58d77e880bfaff52b89b76579ba3242e7d99c54
SonadowB <165947341+SonadowB@users.noreply.github.com>
2025-02-23 22:46:16 -0400

    prac1.java

--- practica1/prac1.java
+++ practica1/prac1.java
@@ -1,1 +1,44 @@
+package ejerjava;
+public class Punto {
+    public float x;
+    public float y;
 
+    public Punto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float[] coord_cartesianas() {
+        return new float[] {this.x, this.y};
+    }
+
+    public float[] coord_polares() {
+        float radio = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+        float angulo = (float) Math.toDegrees(Math.atan2(this.y, this.x));
+        return new float[] {radio, angulo};
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%.2f, %.2f)", this.x, this.y);
+    }
+
+    public static void main(String[] args) {
+        Punto p1 = new Punto(0, 3);
+        System.out.println(p1);
+
+        float[] a = p1.coord_cartesianas();
+        System.out.println(a[0] + " " + a[1]);
+
+        float[] b = p1.coord_polares();
+        System.out.println(b[0] + " " + b[1]);
+    }
+}
