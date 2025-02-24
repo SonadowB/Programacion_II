3c1525c9e7d3e1e5329c3a90f0edb90991433199
SonadowB <165947341+SonadowB@users.noreply.github.com>
2025-02-24 02:02:59 -0400

    Update prac2_circulo.java

--- practica2/prac2_circulo.java
+++ practica2/prac2_circulo.java
@@ -1,1 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
 
+// Clase Punto
+class Punto {
+    int x, y;
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+// Clase Circulo con método para dibujarse
+class Circulo extends JPanel {
+    Punto centro;
+    float radio;
+
+    public Circulo(Punto centro, int radio) {
+        this.centro = centro;
+        this.radio = radio;
+        setPreferredSize(new Dimension(400, 400)); // Tamaño del panel
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawOval(centro.x*10, centro.y*10, 2 * radio * 10, 2 * radio * 10);
+    }
+
+    // Método para mostrar el círculo en una ventana
+    public void dibujaCirculo() {
+        JFrame frame = new JFrame("Dibujar Círculo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+// Clase principal
+public class CirculoG {
+    public static void main(String[] args) {
+        Punto centro = new Punto(2, 2);
+        int radio = 5;
+        Circulo circulo = new Circulo(centro, radio);
+        circulo.dibujaCirculo(); // Muestra la ventana con el círculo
+    }
+}
