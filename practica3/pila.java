f2b06eb545581ac897e20e8d05bcd9986649b4f2
SonadowB <165947341+SonadowB@users.noreply.github.com>
2025-03-12 04:02:27 -0400

    Create pila.java

---
+++ practica3/pila.java
@@ -0,0 +1,23 @@
+public class Pila { 
+private flout arreglo[]: 
+private int top; 
+private int 
+pubite Pila(int n){ 
+this.top = 0; 
+this.nn 
+arreglo new float[n+1]; 
+public void push(float e)( 
+this top this top + 1; 
+arregloftris.top] = 6; 
+publite floot pop() 
+floot dato arreglo[this.top]; 
+This top this.top 1; 
+30 
+31 
+dato; 
+02 
+33 
+34 => 
+public float peek()( 
+35 
+arreglo[this.top]; 
